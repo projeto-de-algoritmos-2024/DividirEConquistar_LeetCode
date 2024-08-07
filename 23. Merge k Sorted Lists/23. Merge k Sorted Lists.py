c33c2590b61c1f0e3c33a8b6ee1d1e53f9d83f06
Harleny <101184511+Angelicahaas@users.noreply.github.com>
2024-08-07 17:28:20 -0300

    implementando heap

--- 23. Merge k Sorted Lists/23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists/23. Merge k Sorted Lists.py
@@ -4,38 +4,14 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
+        
+        # Inserir o primeiro nó de cada lista na heap
+        for i in range(len(lists)):
+            if lists[i]:
+                heappush(heap, (lists[i].val, i, lists[i]))
+        
         head = ListNode(0)  # nó cabeça
         current = head  # ponteiro atual
         
-        # Combinar duas listas
-        while l1 and l2:
-            if l1.val < l2.val:
-                current.next = l1
-                l1 = l1.next
-            else:
-                current.next = l2
-                l2 = l2.next
-            current = current.next
-        
-        # Juntar o resto da lista não vazia
-        current.next = l1 if l1 else l2
-        
-        return head.next
-
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
-            return None
-        if len(lists) == 1:
-            return lists[0]
-        
-        # Combinar listas duas a duas
-        while len(lists) > 1:
-            merged_lists = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if i + 1 < len(lists) else None
-                merged_lists.append(self.mergeTwoLists(l1, l2))
-            lists = merged_lists
-        
-        return lists[0]
