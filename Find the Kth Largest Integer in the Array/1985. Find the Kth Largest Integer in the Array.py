18613068bab5071943b2d8ae12ff89b34db4699d
Harleny <101184511+Angelicahaas@users.noreply.github.com>
2024-08-17 20:27:57 -0300

    Update 1985. Find the Kth Largest Integer in the Array.py

--- Find the Kth Largest Integer in the Array/1985. Find the Kth Largest Integer in the Array.py
+++ Find the Kth Largest Integer in the Array/1985. Find the Kth Largest Integer in the Array.py
@@ -4,7 +4,12 @@ class Solution:
         def mom(arr: List[int], k: int) -> int:
 
             if len(arr) <= 5:
-                return sorted (arr)[k] # retorna os números ordenados e acha a kesima posição
+                return sorted (arr)[k] # ordena e acha a kesima posição
 
         medianas = []
-        
+
+        for i in range(0, len(arr), 5):
+            grupo = sorted(arr[i:i + 5])
+            medianas.append(grupo[len(grupo) // 2]) # 2 é o índice do terceiro elemento, adiciona a mediada calculada na lista
+
+        pivo = mom(medianas, len (medianas) // 2)
