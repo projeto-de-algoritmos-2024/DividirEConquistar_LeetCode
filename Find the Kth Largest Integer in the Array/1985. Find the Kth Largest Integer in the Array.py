83a7f32f64be2b528fe6ef07bdd92f5b55c85f5c
Harleny <101184511+Angelicahaas@users.noreply.github.com>
2024-08-17 19:02:00 -0300

    Update 1985. Find the Kth Largest Integer in the Array.py

--- Find the Kth Largest Integer in the Array/1985. Find the Kth Largest Integer in the Array.py
+++ Find the Kth Largest Integer in the Array/1985. Find the Kth Largest Integer in the Array.py
@@ -1,6 +1,10 @@
-class Solution {
-public:
-    string kthLargestNumber(vector<string>& nums, int k) {
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+
+        def mom(arr: List[int], k: int) -> int:
+
+            if len(arr) <= 5:
+                return sorted (arr)[k] # retorna os números ordenados e acha a kesima posição
+
+        medianas = []
         
-    }
-};
