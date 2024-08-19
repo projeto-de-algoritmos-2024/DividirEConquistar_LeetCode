60eb946e4ccc528314468b0cff51377260182fce
Harleny <101184511+Angelicahaas@users.noreply.github.com>
2024-08-19 16:59:16 -0300

    inicializando uma arvore

--- Create Sorted Array through Instructions/1649. Create Sorted Array through Instructions.py
+++ Create Sorted Array through Instructions/1649. Create Sorted Array through Instructions.py
@@ -1,3 +1,21 @@
+class BIT:
+    # inicializar uma arvore
+    def __init__(self, tam):
+        self.arr = [0] * (tam + 1)
+    
+    def soma(self, i):
+        total = 0
+        while i > 0:
+            total += self.arr[i]
+            i -= i & -i # subtrai o bit menos significativo
+        return total
+    
+    def add(self, i, k):
+        # Adiciona k ao arr
+        while i < len(self.arr):
+            self.arr[i] += k
+            i += i & -i  # Move para o próximo índice usando o bit menos significativo
+            
 class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
         
