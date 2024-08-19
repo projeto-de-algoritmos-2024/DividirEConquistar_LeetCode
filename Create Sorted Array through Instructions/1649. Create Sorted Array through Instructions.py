d1df0dc464b6f841bb3b9ea387762aef9fa7a35c
Harleny <101184511+Angelicahaas@users.noreply.github.com>
2024-08-19 17:50:16 -0300

    Update 1649. Create Sorted Array through Instructions.py

--- Create Sorted Array through Instructions/1649. Create Sorted Array through Instructions.py
+++ Create Sorted Array through Instructions/1649. Create Sorted Array through Instructions.py
@@ -1,3 +1,4 @@
+
 class BIT:
     # inicializar uma arvore
     def __init__(self, tam: int):
@@ -15,7 +16,22 @@ class BIT:
         while i < len(self.arr):
             self.arr[i] += k
             i += i & -i  # move para o próximo índice usando o bit menos significativo
-            
+
 class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
+        mod = 1000000007
+        # Inicializa a árvore com tamanho 100000 + 1
+        bit = BIT(100000 + 1)
+        resultado = 0
         
+        for i, num in enumerate(instructions):
+            # custo de inserção
+            custo_e = bit.soma(num - 1)
+            custo_d = i - bit.soma(num)
+            resultado += min(custo_e, custo_d)
+            resultado %= mod
+            
+
+            bit.add(num, 1)
+        
+        return resultado
